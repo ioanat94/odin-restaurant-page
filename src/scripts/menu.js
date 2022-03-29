c32e2e77fede985365daaeb4297f4b433ccc8f1a
ioanat94 <ioanatiplea94@gmail.com>
2022-03-29 09:27:39 +0300

    Styled menu page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -38,7 +38,7 @@ function Food (name, description, ingredients, image) {
         foodIngredients.classList.add('food-ingredients');
         foodIngredients.textContent = this.ingredients;
         const foodImage = document.createElement('div');
-        foodImage.classList.add('soup-image');
+        foodImage.classList.add('food-image');
         foodImage.appendChild(this.image);
 
         foodContainer.appendChild(foodName);
@@ -51,11 +51,11 @@ function Food (name, description, ingredients, image) {
 }
 
 let fetaChickenSoup = new Food('Feta Chicken Soup', 'Tangy feta and free range chicken combined into chunky soup', 'onion | feta | chicken', fetaChickenSoupImg);
-let ricottaSalad = new Food('Ricotta salad with butter dressing', 'A mouth-watering ricotta salad served with butter dressing', 'cucumber | lettuce | cress | ricotta | butter', ricottaSaladImg);
+let ricottaSalad = new Food('Ricotta salad', 'A mouth-watering ricotta salad served with butter dressing', 'cucumber | lettuce | cress | ricotta | butter', ricottaSaladImg);
 let lambKorma = new Food('Rambutan and lamb korma', 'Mild korma made with fresh rambutan and succulent lamb', 'lamb | onion | peppers | garlic | ginger | tumeric | coriander', lambKormaImg);
 let mangoVindaloo = new Food('Mango and spinach vindaloo', 'Spicy vindaloo made with dried mango and fresh spinach', 'mango | spinach | onion | garlic | ginger | cayenne', mangoVindalooImg);
-let camembertCrepes = new Food('Camembert and hazelnut crepes', 'Crispy crepes filled with camembert and hazelnut', 'flour | butter |  egg | milk | camembert | hazelnut', camembertCrepesImg);
-let chocolateCheesecake = new Food('Chocolate and mascarpone cheesecake', 'A velvety cheesecake layered with plain chocolate and mascarpone', 'vanilla | double cream | mascarpone | chocolate', chocolateCheesecakeImg);
+let camembertCrepes = new Food('Camembert hazelnut crepes', 'Crispy crepes filled with camembert and hazelnut', 'flour | butter |  egg | milk | camembert | hazelnut', camembertCrepesImg);
+let chocolateCheesecake = new Food('Chocolate cheesecake', 'A velvety cheesecake layered with plain chocolate and mascarpone', 'vanilla | double cream | mascarpone | chocolate', chocolateCheesecakeImg);
 
 export default function loadMenuPage() {
     content.classList.add('content-menu');
